df789dc70042b1795681a735e04eee1e89208280
me-rodev <100472669+me-rodev@users.noreply.github.com>
2022-02-27 17:22:37 +0100

    code clean up

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -11,4 +11,3 @@ console.log(noOfElements([2, 3, 0, 1, 7], 4, 8));
 console.log(noOfElements([3, 3, 3, 5, 18, 6, 20], 15, 30));
 console.log(noOfElements([4, 6, 2, 6, 7, 7], 0, 7));
 module.exports = noOfElements;
-console.log("TEST");
